44a7485b11fd9471535da910a8a053f5d0d5ace3
Oung Seik <mhemaungthuwin@gmail.com>
2025-12-10 20:24:59 +0630

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            biome
+
+            cargo-audit
+            cargo-nextest
+            cargo-tarpaulin
+            otel-desktop-viewer
+            sqlx-cli
+          ];
+
+          DATABASE_URL = "sqlite:./database/expanse-tracker.sqlite";
+          OTEL_EXPORTER_OTLP_ENDPOINT = "http://localhost:4318";
+          OTEL_TRACES_EXPORTER = "otlp";
+          OTEL_EXPORTER_OTLP_PROTOCOL = "http/protobuf";
+          STATIC_ASSETS_FOLDER = "static";
+          RUSTUP_TOOLCHAIN = "stable";
+
+          LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+        };
+      }
+    );
+}
