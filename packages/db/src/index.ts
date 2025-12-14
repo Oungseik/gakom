377a8d03b5f1e1a96be9f07c54164df47f2c2767
Oung Seik <mhemaungthuwin@gmail.com>
2025-12-14 13:14:08 +0630

    refactor: connect database

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,30 +1,11 @@
 import { Database } from "@tursodatabase/database";
-import {
-  and,
-  avg,
-  count,
-  DrizzleQueryError,
-  desc,
-  eq,
-  gt,
-  gte,
-  ilike,
-  inArray,
-  like,
-  lt,
-  lte,
-  not,
-  or,
-} from "drizzle-orm";
 import { drizzle } from "drizzle-orm/tursodatabase/database";
 import { account, session, user, verification } from "./schema/core";
 import { invitation, member, organization } from "./schema/organization";
 import { relations } from "./schema/relations";
 
-let _: ReturnType<typeof connect> | undefined;
-
-function connect() {
-  const client = new Database(process.env.DATABASE_URL!);
+export function connect(url: string) {
+  const client = new Database(url);
   return drizzle({
     client,
     schema: { user, session, account, verification, invitation, member, organization },
@@ -32,31 +13,5 @@ function connect() {
   });
 }
 
-function init() {
-  if (!_) {
-    _ = connect();
-  }
-  return _;
-}
-
-export const db = init();
-
 export { account, session, user, verification, invitation, member, organization };
 
-export {
-  eq,
-  not,
-  and,
-  or,
-  lt,
-  lte,
-  gt,
-  gte,
-  like,
-  ilike,
-  desc,
-  avg,
-  count,
-  inArray,
-  DrizzleQueryError,
-};
