8f7e3226397d04d7a20b1e4579d8bb34a91709ff
Oung Seik <mhemaungthuwin@gmail.com>
2025-12-14 12:20:06 +0630

    chore: delete web-app package

--- apps/web-app/src/lib/auth_client.ts
+++
@@ -1,7 +0,0 @@
-import { emailOTPClient } from "better-auth/client/plugins";
-import { createAuthClient } from "better-auth/svelte";
-import {PUBLIC_BETTER_AUTH_BASE_URL} from "$env/static/public";
-
-export const authClient = createAuthClient({
-  plugins: [emailOTPClient()],
-});
--- apps/web-app/src/lib/server/rate-limit.ts
+++
@@ -1,9 +0,0 @@
-import { RateLimiterMemory } from "rate-limiter-flexible";
-
-const opts = {
-	points: 20,
-	/** per second */
-	duration: 1,
-};
-
-export const rateLimiter = new RateLimiterMemory(opts);
