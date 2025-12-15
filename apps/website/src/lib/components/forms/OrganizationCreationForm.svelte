ddd74ac9b4da2df8ba1a008d52d71e94f8647971
Oung Seik <mhemaungthuwin@gmail.com>
2025-12-15 20:31:20 +0630

    feat: add sonner

--- apps/website/src/lib/components/forms/OrganizationCreationForm.svelte
+++ apps/website/src/lib/components/forms/OrganizationCreationForm.svelte
@@ -1,45 +1,52 @@
 <script lang="ts">
   import Loader2Icon from "@lucide/svelte/icons/loader-2";
   import { Button } from "@repo/ui/button";
+  import * as ImageCropper from "@repo/ui/image-cropper";
+  import { getFileFromUrl } from "@repo/ui/image-cropper";
   import { Input } from "@repo/ui/input";
   import { Label } from "@repo/ui/label";
   import { createForm } from "@tanstack/svelte-form";
+  import { createMutation } from "@tanstack/svelte-query";
   import { toast } from "svelte-sonner";
   import z from "zod";
 
   import { authClient } from "$lib/auth_client";
+  import { orpc } from "$lib/orpc_client";
 
   type Props = {
-    onSubmit?: (param: { name: string; slug: string; logo?: string }) => void;
+    onSubmit?: (param: { name: string; slug: string; logo: string }) => void;
     onSubmitError?: () => void;
   };
 
   let isSubmitting = $state(false);
   const { onSubmit, onSubmitError }: Props = $props();
+  let logo: string | undefined = $state(undefined);
+
+  const imageUploadMutation = createMutation(() => orpc.images.upload.mutationOptions());
 
   const form = createForm(() => ({
-    defaultValues: { name: "", slug: "", logo: "" },
+    defaultValues: { name: "", slug: "" },
     onSubmit: async ({ value }) => {
       isSubmitting = true;
-
       const { data, error } = await authClient.organization.checkSlug({ slug: value.slug });
 
       if (error || data.status) {
         isSubmitting = false;
-        toast(`The slug "${value.slug} is already taken"`);
+        toast(`The slug "${value.slug} is already taken."`);
         return;
       }
 
+      if (!logo) {
+        isSubmitting = false;
+        toast(`Please upload a logo of your organization.`);
+        return;
+      }
+
+      const submitParam = { ...value, logo };
       await authClient.organization.create(
+        { name: value.name, slug: value.slug },
         {
-          name: value.name,
-          slug: value.slug,
-          logo: value.logo,
-        },
-        {
-          onSuccess: () => {
-            onSubmit?.(value);
-          },
+          onSuccess: () => onSubmit?.(submitParam),
           onError: ({ error }) => {
             toast.error(error.message);
             onSubmitError?.();
@@ -59,6 +66,41 @@
     form.handleSubmit();
   }}
 >
+  <div class="mx-auto w-fit">
+    <ImageCropper.Root
+      onCropped={async (url) => {
+        isSubmitting = true;
+        const file = await getFileFromUrl(url);
+        imageUploadMutation.mutate(
+          { file },
+          {
+            onSuccess: (data) => {
+              logo = data.objectPath;
+            },
+            onError: (e) => toast.error(e.message),
+            onSettled: () => {
+              isSubmitting = false;
+            },
+          }
+        );
+      }}
+      onUnsupportedFile={(file) => {
+        toast.error(`Unsupported file type: ${file.type}`);
+      }}
+    >
+      <ImageCropper.UploadTrigger>
+        <ImageCropper.Preview class="rounded-md" />
+      </ImageCropper.UploadTrigger>
+      <ImageCropper.Dialog>
+        <ImageCropper.Cropper cropShape="rect" />
+        <ImageCropper.Controls>
+          <ImageCropper.Cancel />
+          <ImageCropper.Crop />
+        </ImageCropper.Controls>
+      </ImageCropper.Dialog>
+    </ImageCropper.Root>
+  </div>
+
   <div class="space-y-4">
     <form.Field
       name="name"
@@ -91,7 +133,12 @@
       name="slug"
       validators={{
         onChange: ({ value }) =>
-          z.string().min(1, "Slug is required").safeParse(value).error?.issues?.at(0)?.message,
+          z
+            .string()
+            .min(1, "Slug is required")
+            .max(20, "Slug is too long")
+            .safeParse(value)
+            .error?.issues?.at(0)?.message,
       }}
     >
       {#snippet children(field)}
@@ -114,35 +161,7 @@
       {/snippet}
     </form.Field>
 
-    <form.Field
-      name="logo"
-      validators={{
-        onChange: ({ value }) => {
-          if (!value) return undefined;
-          return z.url("Invalid URL").safeParse(value).error?.issues?.at(0)?.message;
-        },
-      }}
-    >
-      {#snippet children(field)}
-        <div class="space-y-2">
-          <Label for={field.name}>Logo URL (optional)</Label>
-          <Input
-            id={field.name}
-            name={field.name}
-            value={field.state.value}
-            onblur={field.handleBlur}
-            onchange={(e) => field.handleChange(e.currentTarget.value)}
-            placeholder="https://example.com/logo.png"
-            disabled={isSubmitting}
-          />
-          {#if field.state.meta.errors.length}
-            <p class="text-sm text-red-500">{field.state.meta.errors}</p>
-          {/if}
-        </div>
-      {/snippet}
-    </form.Field>
-
-    <Button disabled={isSubmitting} type="submit" class="w-full">
+    <Button disabled={isSubmitting} type="submit" class="mt-2 w-full">
       {#if isSubmitting}
         <Loader2Icon class="animate-spin" />
       {:else}
--- apps/website/src/routes/(app)/setup/+page.svelte
+++ apps/website/src/routes/(app)/setup/+page.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
+  import * as Card from "@repo/ui/card";
+
   import OrganizationCreationForm from "$lib/components/forms/OrganizationCreationForm.svelte";
 </script>
 
-<OrganizationCreationForm />
+<div class="mt-20 flex min-h-svh flex-col items-center gap-6 p-6 md:p-10">
+  <div class="flex w-full max-w-md flex-col gap-6">
+    <Card.Root>
+      <Card.Content>
+        <OrganizationCreationForm />
+      </Card.Content>
+    </Card.Root>
+
+    <p class="text-muted-foreground text-center">
+      Enter your information below to create an organization
+    </p>
+  </div>
+</div>
