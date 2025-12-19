3a94059ebebcd9982c7c057b8b989b451c9fef74
Oung Seik <mhemaungthuwin@gmail.com>
2025-12-19 19:42:36 +0630

    fix: redirect to first organizations if go to /dashboard

---
+++ apps/website/src/routes/(private)/dashboard/+page.server.ts
@@ -0,0 +1,9 @@
+import { redirect } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async ({ parent }) => {
+  const { organizations } = await parent();
+  const slug = organizations.at(0)?.slug;
+
+  redirect(303, slug ? `/dashboard/${slug}` : "/setup");
+};
