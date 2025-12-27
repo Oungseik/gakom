7013a97cddd6e7f51f82ac0f248517268b697e8b
Oung Seik <mhemaungthuwin@gmail.com>
2025-12-27 18:24:21 +0630

    refactor: reorganize files

--- apps/website/src/lib/server/orpc/router.ts
+++ apps/website/src/lib/server/orpc/router.ts
@@ -4,15 +4,15 @@ import { os } from "./base";
 import { healthCheckHandler } from "./handlers/health/health_check";
 import { removeImageHandler } from "./handlers/images/remove_image";
 import { uploadImageHandler } from "./handlers/images/upload_image";
-import { countAttendancePoliciesHandler } from "./handlers/organizations/countAttendancePolicies";
-import { createAttendancePolicyHandler } from "./handlers/organizations/createAttendancePolicy";
-import { deleteAttendancePolicyHandler } from "./handlers/organizations/deleteAttendancePolicy";
-import { getAttendancePolicyHandler } from "./handlers/organizations/getAttendancePolicy";
-import { listAttendancePoliciesHandler } from "./handlers/organizations/listAttendancePolicies";
-import { listInvitationsHandler } from "./handlers/organizations/listInvitations";
-import { listMembersHandler } from "./handlers/organizations/listMembers";
-import { updateAttendancePolicyHandler } from "./handlers/organizations/updateAttendancePolicy";
-import { updateMemberHandler } from "./handlers/organizations/updateMember";
+import { countAttendancePoliciesHandler } from "./handlers/organizations/attendancePolicies/count";
+import { createAttendancePolicyHandler } from "./handlers/organizations/attendancePolicies/create";
+import { deleteAttendancePolicyHandler } from "./handlers/organizations/attendancePolicies/delete";
+import { getAttendancePolicyHandler } from "./handlers/organizations/attendancePolicies/get";
+import { listAttendancePoliciesHandler } from "./handlers/organizations/attendancePolicies/list";
+import { updateAttendancePolicyHandler } from "./handlers/organizations/attendancePolicies/update";
+import { listInvitationsHandler } from "./handlers/organizations/invitations/list";
+import { listMembersHandler } from "./handlers/organizations/members/list";
+import { updateMemberHandler } from "./handlers/organizations/members/update";
 
 export const router = os.router({
   health: { check: healthCheckHandler },
