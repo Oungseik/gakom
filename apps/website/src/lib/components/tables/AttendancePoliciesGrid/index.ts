ac198b7b957f6aa1210688593513002a15768acc
Oung Seik <mhemaungthuwin@gmail.com>
2025-12-27 12:41:09 +0630

    refactor: remove unused components

--- apps/website/src/lib/components/tables/AttendancePoliciesGrid/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AttendancePoliciesGrid } from "./AttendancePoliciesGrid.svelte";
-export { default as PolicyCard } from "./PolicyCard.svelte";
-export type { AttendancePolicy } from "./types";
-
