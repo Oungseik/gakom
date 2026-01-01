d38048778b13d1611abeb6e675fd67c70a4938c1
Oung Seik <mhemaungthuwin@gmail.com>
2026-01-01 19:06:21 +0630

    feat: add app and dashboard error page

---
+++ apps/website/src/routes/(private)/app/[slug]/+layout.server.ts
@@ -0,0 +1,15 @@
+import { error } from "@sveltejs/kit";
+import type { LayoutServerLoad } from "./$types";
+
+export const load: LayoutServerLoad = async ({ parent, params }) => {
+  const { organizations } = await parent();
+  const currentOrganization = organizations?.find((o) => o.slug === params.slug);
+
+  if (!currentOrganization) {
+    return error(404);
+  }
+
+  return {
+    currentOrganization,
+  };
+};
