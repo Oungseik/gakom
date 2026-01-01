41e4a0921cc8f7f030e1651035da8aef0d1cef93
Oung Seik <mhemaungthuwin@gmail.com>
2026-01-01 13:22:22 +0630

    fix: fix type error

--- apps/website-tests/tests/auth/forgot_password.spec.ts
+++ apps/website-tests/tests/auth/forgot_password.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from "@playwright/test";
 import { config } from "../config";
 
 test("forgot password", async ({ page }) => {
-  const forgotPasswordURL = `${config.BASE_URL}/forgot-password`;
+  const forgotPasswordURL = `${config.baseUrl}/forgot-password`;
 
   await page.goto(forgotPasswordURL);
   expect(page.url()).toEqual(forgotPasswordURL);
