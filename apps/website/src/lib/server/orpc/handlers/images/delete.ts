d5ded3912f0c7e74571df642518692127a44e7ce
Oungseik <59136796+Oungseik@users.noreply.github.com>
2026-02-07 10:49:32 +0630

    feat: upload and delete image (#1)

---
+++ apps/website/src/lib/server/orpc/handlers/images/delete.ts
@@ -0,0 +1,22 @@
+import { and, eq, image } from "@repo/db";
+import z from "zod";
+import { STORAGE_PUBLIC_URL } from "$env/static/private";
+import { db } from "$lib/server/db";
+import { authMiddleware, os } from "$lib/server/orpc/base";
+import { removeImage } from "$lib/server/utils/storage";
+
+const input = z.object({
+  objectPath: z.string(),
+});
+
+export const deleteImageHandler = os
+  .use(authMiddleware)
+  .input(input)
+  .handler(async ({ input, context }) => {
+    await db
+      .delete(image)
+      .where(
+        and(eq(image.objectPath, input.objectPath), eq(image.uploaderId, context.session.user.id)),
+      );
+    await removeImage(input.objectPath.slice(STORAGE_PUBLIC_URL.length + 1));
+  });
--- apps/website/src/lib/server/orpc/router.ts
+++ apps/website/src/lib/server/orpc/router.ts
@@ -2,9 +2,15 @@ import { createRouterClient } from "@orpc/server";
 
 import { os } from "./base";
 import { healthCheckHandler } from "./handlers/health/health_check";
+import { deleteImageHandler } from "./handlers/images/delete";
+import { uploadImageHandler } from "./handlers/images/upload";
 
 export const router = os.router({
   health: { check: healthCheckHandler },
+  images: {
+    upload: uploadImageHandler,
+    delete: deleteImageHandler,
+  },
 });
 
 /** only for server-side call */
