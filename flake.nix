37b3ca5d813d1b4a83283e4b9b5076e12950d17c
Oung Seik <mhemaungthuwin@gmail.com>
2026-02-16 14:13:26 +0630

    Squashed commit of the following:
    commit 40ea40ec004512bd5050a0a173bf8c4cb3be282c
    Author: Oung Seik <mhemaungthuwin@gmail.com>
    Date:   Mon Feb 16 08:39:38 2026 +0630
    
        chore: remove playwright flake
    
    commit 9c3a8be7f79840900cff6a7cd899be9c0fe0a4aa
    Author: Oung Seik <mhemaungthuwin@gmail.com>
    Date:   Sun Feb 15 17:15:10 2026 +0630
    
        feat: add coming soon for unimplemented features
    
    commit 7eeae65ef0c01836383eb649c6375291fc733be7
    Author: Oung Seik <mhemaungthuwin@gmail.com>
    Date:   Sun Feb 15 17:04:07 2026 +0630
    
        feat: implement app/leave page
    
    commit def9165f13c9436997e6395fed927f489da9c87a
    Author: Oung Seik <mhemaungthuwin@gmail.com>
    Date:   Sun Feb 15 16:30:50 2026 +0630
    
        feat: implement leave request page.
    
    commit 42e077469907bbfb79ce4fc52617ba9cb1ae6268
    Author: Oung Seik <mhemaungthuwin@gmail.com>
    Date:   Sun Feb 15 15:13:15 2026 +0630
    
        feat: polish ui
    
    commit 6047def5fd9cad05b37908ac47c4630ad4db1f1c
    Author: Oung Seik <mhemaungthuwin@gmail.com>
    Date:   Sun Feb 15 15:00:44 2026 +0630
    
        feat: remove table mobile UI
    
    commit c91541846d06a7681fbda33210567e23360a8812
    Author: Oung Seik <mhemaungthuwin@gmail.com>
    Date:   Sun Feb 15 14:28:46 2026 +0630
    
        feat: attendance history page
    
    commit 6e3bd855be52f228b83afe2eb9ea509d81826161
    Author: Oung Seik <mhemaungthuwin@gmail.com>
    Date:   Sun Feb 15 13:50:41 2026 +0630
    
        fix: add separator

--- flake.nix
+++ flake.nix
@@ -2,54 +2,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
-    playwright.url = "github:pietdevries94/playwright-web-flake";
   };
   outputs =
     {
       self,
       nixpkgs,
       utils,
-      playwright,
     }:
     utils.lib.eachDefaultSystem (
       system:
       let
-        overlay = final: prev: {
-          inherit (playwright.packages.${system}) playwright-test playwright-driver;
-        };
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ overlay ];
-        };
+        pkgs = nixpkgs.legacyPackages.${system};
       in
       {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             biome
-            turso
-
-            pkgs.playwright-test
-
-            cargo-audit
-            cargo-nextest
-            cargo-tarpaulin
-            otel-desktop-viewer
-            sqlx-cli
           ];
-
-          PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = 1;
-          PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
-          PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = true;
-          PLAYWRIGHT_NODEJS_PATH = "${pkgs.nodejs_24}/bin/node";
-          PLAYWRIGHT_HOST_PLATFORM_OVERRIDE = "";
-          PLAYWRIGHT_LAUNCH_OPTIONS_EXECUTABLE_PATH = "${pkgs.playwright.browsers}/chromium-1181/chrome-linux/chrome";
-
-          OTEL_EXPORTER_OTLP_ENDPOINT = "http://localhost:4318";
-          OTEL_TRACES_EXPORTER = "otlp";
-          OTEL_EXPORTER_OTLP_PROTOCOL = "http/protobuf";
-
-          STATIC_ASSETS_FOLDER = "static";
-          RUSTUP_TOOLCHAIN = "stable";
 
           LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
         };
